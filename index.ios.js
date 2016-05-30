c359568af59f0403e09de9321b912a225a1d4933
Anton Stepan <info@antonstepan.it>
2016-05-30 23:44:28 +0200

    ios navigator

--- index.ios.js
+++ index.ios.js
@@ -9,111 +9,35 @@ import {
   AppRegistry,
   Image,
   ListView,
+  NavigatorIOS,
   StyleSheet,
   Text,
   TouchableHighlight,
   View
 } from 'react-native';
 
+import HeroList from './App/Components/HeroList';
+
 const heroes = require('./data/heroes.json');
 const ROOT_URL = 'https://overwatch-data.herokuapp.com/img/heroes';
 
 class overwatchapp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData() {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(heroes),
-      loaded: true
-    });
-  }
-
   render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderHero.bind(this)}
-        style={styles.listView}
-      />
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Overheroes',
+          component: HeroList,
+        }}/>
     );
   }
-
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading ...
-        </Text>
-      </View>
-    );
-  }
-
-  renderHero(hero, sectionId, rowId) {
-    return (
-      <TouchableHighlight onPress={() => this._pressRow(rowId, hero)}>
-        <View style={styles.container}>
-          <Image
-            source={{uri: `${ROOT_URL}/${hero.picture}`}}
-            style={styles.thumbnail}
-          />
-          <View style={styles.rightContainer}>
-            <Text style={styles.name}>{hero.name}</Text>
-            <Text style={styles.class}>{hero.class}</Text>
-          </View>
-        </View>
-      </TouchableHighlight>
-    );
-  }
-
-  _pressRow(rowId, hero) {
-    console.log(`Pressed on ${rowId} with hero: `, hero);
-  }
-
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  name: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  class: {
-    textAlign: 'center',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('overwatchapp', () => overwatchapp);
