86e9bc97ff8e2d804dcb2e204ef53c2a0faa257f
Anton Stepan <info@antonstepan.it>
2016-05-30 23:19:26 +0200

    heroes with icon

--- index.ios.js
+++ index.ios.js
@@ -7,25 +7,73 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+const heroes = require('./data/heroes.json');
+const ROOT_URL = 'https://overwatch-data.herokuapp.com/img/heroes';
+
 class overwatchapp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(heroes),
+      loaded: true
+    });
+  }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderHero}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text>
+          Loading ...
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      </View>
+    );
+  }
+
+  renderHero(hero) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: `${ROOT_URL}/${hero.picture}`}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.name}>{hero.name}</Text>
+          <Text style={styles.class}>{hero.class}</Text>
+        </View>
       </View>
     );
   }
@@ -34,19 +82,29 @@ class overwatchapp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  name: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  class: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
