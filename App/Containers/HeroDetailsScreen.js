3e2178aa30a7fb181f224eed3de986c02a751301
Anton Stepan <info@antonstepan.it>
2016-06-07 20:50:04 +0200

    moved old details to this branch

--- App/Containers/HeroDetailsScreen.js
+++ App/Containers/HeroDetailsScreen.js
@@ -1,7 +1,8 @@
 import React, { PropTypes } from 'react'
-import { ScrollView, Text } from 'react-native'
+import { Image, ScrollView, View, Text } from 'react-native'
 
 import styles from './Styles/HeroDetailsScreenStyle'
+const ROOT_URL = 'https://overwatch-data.herokuapp.com/img/heroes'
 
 export default class HeroDetailsScreen extends React.Component {
 
@@ -17,10 +18,44 @@ export default class HeroDetailsScreen extends React.Component {
   }
 
   render () {
+    const { hero } = this.props
+
+    // TODO extract to method, with big switch case on different ability props
+    const abilities = hero.abilities.map(a => {
+      const imageUrl = a.image ? `${ROOT_URL}/${a.image}` : ''
+      return <View key={a.name} style={styles.ability}>
+        <Image
+          source={{uri: imageUrl}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.abilityDetails}>
+          <Text>{a.name}</Text>
+          <Text>{a.damage}</Text>
+        </View>
+      </View>
+    })
+
     return (
       <ScrollView style={styles.container}>
-        <Text>HeroDetailsScreen Container</Text>
-        <Text>{this.props.hero.name}</Text>
+        <Image
+          source={{uri: `${ROOT_URL}/${hero.picture}`}}
+          style={styles.image}
+        />
+        <View style={styles.block}>
+          <View style={styles.heading}>
+            <View style={styles.title}>
+              <Text style={styles.name}>{hero.name}</Text>
+            </View>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.desc}>HP: {hero.hp}</Text>
+            <Text style={styles.desc}>Armor: {hero.armor}</Text>
+            <Text style={styles.desc}>Difficulty: {hero.difficulty}</Text>
+          </View>
+        </View>
+        <View style={styles.block}>
+          {abilities}
+        </View>
       </ScrollView>
     )
   }
